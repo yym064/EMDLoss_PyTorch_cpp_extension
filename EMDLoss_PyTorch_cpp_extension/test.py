8f36185216ba3abc499a099770a9f3663b8e1b6b
yoonmo <yym064@naver.com>
2022-05-20 00:30:28 +0900

    change test script a little bit

--- EMDLoss_PyTorch_cpp_extension/test.py
+++ EMDLoss_PyTorch_cpp_extension/test.py
@@ -3,11 +3,29 @@ import dist_emd
 
 if __name__ == "__main__":
 
-    a = torch.randn(5, 50, 3).cuda()
-    b = torch.randn(5, 50, 3).cuda()
+    # forward test
+    a = torch.randn(5, 50, 3, requires_grad = True).cuda()
+    b = torch.randn(5, 50, 3, requires_grad = True).cuda()
 
     print(f'input shape {a.size()}: ')
     print(f'target shape {b.size()}: ')
     cost = dist_emd.earth_mover_distance(a, b)
     print(f'cost shape {cost.size()}: ')
+
+    # backward test
+    model = torch.nn.Sequential(
+        torch.nn.Linear(150, 150),
+        torch.nn.ReLU(),
+        torch.nn.Linear(150, 150)
+    )
+
+    model.cuda()
+
+    a = a.view(5, -1)
+    a_ = model(a)
+    a_ = a_.view(5, 50, -1)
+
+    cost = (dist_emd.earth_mover_distance(a_, b)).sum()
+    cost.backward()
+    print(f'backward is done')
 
